ed7c87c42ea613ab7154e2faa59cdc14879f9656
Yanjuan1995 <li.yanj@husky.neu.edu>
2019-02-07 15:39:25 -0500

    setup and teardown

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -1,7 +1,4 @@
-set -e
-
-
-#Usage: setting up our networking resources such as Virtual Private Cloud (VPC), Internet Gateway, Route Table and Routes
+#!/usr/bin/env bash
 
 #Arguments: STACK_NAME
 
@@ -11,6 +8,18 @@ vpcId=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId' --output 
 #Tag vpc
 aws ec2 create-tags --resources $vpcId --tags Key=Name,Value=$STACK_NAME-csye6225-vpc
 echo "Vpc created-> Vpc Id:  "$vpcId
+
+#Create subnets
+subnetId=`aws ec2 create-subnet --vpc-id $vpcId --cidr-block 10.0.0.0/24 --query 'Subnet.SubnetId' --output text`
+#Tag subnet
+aws ec2 create-tags --resources $subnetId --tags Key=Name,Value=$STACK_NAME-csye6225-subnet
+
+subnetId2=`aws ec2 create-subnet --vpc-id $vpcId --cidr-block 10.0.1.0/24 --query 'Subnet.SubnetId' --output text`
+aws ec2 create-tags --resources $subnetId2 --tags Key=Name,Value=$STACK_NAME-csye6225-subnet2
+
+subnetId3=`aws ec2 create-subnet --vpc-id $vpcId --cidr-block 10.0.2.0/24 --query 'Subnet.SubnetId' --output text`
+aws ec2 create-tags --resources $subnetId3 --tags Key=Name,Value=$STACK_NAME-csye6225-subnet3
+#aws ec2 create-subnet --vpc-id $vipId --cidr-block 10.0.2.0/24
 
 #Create Internet Gateway
 gatewayId=`aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text`
@@ -25,14 +34,29 @@ echo "Attached Internet gateway: "$gatewayId" to Vpc: "$vpcId
 #Create Route Table
 routeTableId=`aws ec2 create-route-table --vpc-id $vpcId --query 'RouteTable.RouteTableId' --output text`
 #Tag Route Table
-aws ec2 create-tags --resources $routeTableId --tags Key=Name,Value=$STACK_NAME-csye6225-public-route-table
+aws ec2 create-tags --resources $routeTableId --tags Key=Name,Value=$STACK_NAME-csye6225-rt
 echo "Route table created -> route table Id: "$routeTableId
 
 #Create Route
 aws ec2 create-route --route-table-id $routeTableId --destination-cidr-block 0.0.0.0/0 --gateway-id $gatewayId
 echo "Route created: in "$routeTableId" target to "$gatewayId
-#Job Done
-echo "Job Done!"
+
+#Attach route tables to subnets
+aws ec2 associate-route-table --subnet-id $subnetId --route-table-id $routeTableId
+aws ec2 associate-route-table --subnet-id $subnetId2 --route-table-id $routeTableId
+aws ec2 associate-route-table --subnet-id $subnetId3 --route-table-id $routeTableId
+
+#Create security group
+aws ec2 create-security-group --group-name MySecurityGroup description "My security group" --vpc-id $vpcId
+aws ec2 revoke-security-group-ingress --group-name MySecurityGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 revoke-security-group-ingress --group-name MySecurityGroup --protocol tcp --port 80 --cidr 0.0.0.0/0
+
+#Done
+echo "Done!"
+
+
+
+
 
 
 
