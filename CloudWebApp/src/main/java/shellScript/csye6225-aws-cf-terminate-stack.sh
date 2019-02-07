30574576432d77ddb28bd42e70ff7d7e66a459c0
Yanjuan1995 <li.yanj@husky.neu.edu>
2019-02-06 21:34:39 -0500

    Add Status

--- CloudWebApp/src/main/java/shellScript/csye6225-aws-cf-terminate-stack.sh
+++ CloudWebApp/src/main/java/shellScript/csye6225-aws-cf-terminate-stack.sh
@@ -1,13 +1,50 @@
 #!/usr/bin/env bash
 
-set -e
+#set -e
 
-echo "The stack you want to delete : "
+#echo "The stack you want to delete : "
 
 #Delete stack
-aws cloudformation delete-stack --stack-name STACK_NAME-csye6225-vpc
-aws cloudformation delete-stack --stack-name STACK_NAME-csye6225-rt
+#aws cloudformation delete-stack --stack-name STACK_NAME-csye6225-vpc
+#aws cloudformation delete-stack --stack-name STACK_NAME-csye6225-rt
 
 #Success
-echo "Delete successfully!"
+#echo "Delete successfully!"
 
+
+#Arguments: STACK_NAME
+STACK_NAME=$1
+#Get a vpc-Id using the name provided
+vpcId=`aws ec2 describe-vpcs --filter "Name=tag:Name,Values=${STACK_NAME}-csye6225-vpc" --query 'Vpcs[*].{id:VpcId}' --output text`
+#Get a Internet Gateway Id using the name provided
+gatewayId=`aws ec2 describe-internet-gateways --filter "Name=tag:Name,Values=${STACK_NAME}-csye6225-InternetGateway" --query 'InternetGateways[*].{id:InternetGatewayId}' --output text`
+#Get a route table Id using the name provided
+routeTableId=`aws ec2 describe-route-tables --filter "Name=tag:Name,Values=${STACK_NAME}-csye6225-public-route-table" --query 'RouteTables[*].{id:RouteTableId}' --output text`
+#Get subnet Id using the name provided
+subnetId=`aws ec2 describe-subnets --filter "Name=tag:Name,Values=${STACK_NAME}-csye6225-subnet" --query 'Subnets[*].{id:SubnteId}' --output text`
+
+#Delete the route
+aws ec2 delete-route --route-table-id $routeTableId --destination-cidr-block 0.0.0.0/0
+echo "Deleting the route..."
+
+#Delete the subnets
+aws ec2 delete-subnet --subnet-id $subnetId
+echo "Deleting the subnets..."
+
+#Delete the route table
+aws ec2 delete-route-table --route-table-id $routeTableId
+echo "Deleting the route table-> route table id: "$routeTableId
+
+#Detach Internet gateway and vpc
+aws ec2 detach-internet-gateway --internet-gateway-id $gatewayId --vpc-id $vpcId
+echo "Detaching the Internet gateway from vpc..."
+
+#Delete the Internet gateway
+aws ec2 delete-internet-gateway --internet-gateway-id $gatewayId
+echo "Deleting the Internet gateway-> gateway id: "$gatewayId
+
+#Delete the vpc
+aws ec2 delete-vpc --vpc-id $vpcId
+echo "Deleting the vpc-> vpc id: "$vpcId
+
+echo "Job done!"
