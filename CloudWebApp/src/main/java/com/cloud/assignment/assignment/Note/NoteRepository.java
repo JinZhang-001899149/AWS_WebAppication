d471e8461e1eeb9e76588e22c699a3809adeda71
Yanjuan1995 <li.yanj@husky.neu.edu>
2019-02-13 15:15:49 -0500

    update

---
+++ CloudWebApp/src/main/java/com/cloud/assignment/assignment/Note/NoteRepository.java
@@ -0,0 +1,7 @@
+package com.cloud.assignment.assignment.Note;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface NoteRepository extends CrudRepository<Note,Integer>{
+
+}
