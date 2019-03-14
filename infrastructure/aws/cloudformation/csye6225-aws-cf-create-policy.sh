1f21b12e51e5447e652a0a4eae1aef8f6a6467cb
Jingyi Cui <cui.jingy@husky.neu.edu>
2019-03-14 15:27:45 -0400

    Update circleci

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-policy.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-policy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 stackname=$1
-userName=$2
+UserName=$2
 s3=$3
 s3bucket="arn:aws:s3:::code-deploy.csye6225-spring2019-"$s3".me/*"
 
 aws cloudformation create-stack --stack-name $stackname 
 --capabilities CAPABILITY_NAMED_IAM --template-body 
 file://./csye6225-cf-policy.json --parameters 
-ParameterKey=circleci,ParameterValue=$userName
+ParameterKey=circleci,ParameterValue=$UserName
 ParameterKey=bucketArn,ParameterValue=$s3bucket||{"command failed";exit 1;}
 aws cloudformation wait stack-create-complete --stack-name $stackname
 
