f302e95377002911901a7d76d598fe45b9c0bc50
JinZhang-001899149 <zhang.jin2@husky.neu.edu>
2019-03-20 19:00:20 -0400

    123

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -2,5 +2,5 @@
 sudo service tomcat stop
 kill -9 $(lsof -i:8080 | awk '{print $2}' | tail -n 2)
 cd /home/centos
-sudo mkdir -p CloudWebApp/traget/classes
+sudo mkdir -p CloudWebApp/target/classes
 sudo chmod 777 -R CloudWebApp/target/classes
