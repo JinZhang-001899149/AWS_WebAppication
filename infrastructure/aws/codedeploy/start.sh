f25669bce8c1653261e60578db4878def73cf410
Jingyi Cui <cui.jingy@husky.neu.edu>
2019-03-20 15:29:14 -0400

    Update user data

--- infrastructure/aws/codedeploy/start.sh
+++ infrastructure/aws/codedeploy/start.sh
@@ -3,7 +3,7 @@ cd /home/centos
 url=$(cat mysqlsetting.txt | sed -r 's/.*"(.+)".*/\1/')
 echo $url
 
-
-
-java -jar /home/centos/CloudWebApp/target/assignment01-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.username=csye6225master --spring.datasource.password=csye6225password --spring.datasource.url=jdbc:mysql://$url/csye6225 --server.port=80 >/dev/null 2>&1 &
+cd /home/centos/CloudWebApp/target
+pwd
+java -jar assignment01-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.username=csye6225master --spring.datasource.password=csye6225password --spring.datasource.url=jdbc:mysql://$url/csye6225 --server.port=8080 >/dev/null 2>&1 &
 
