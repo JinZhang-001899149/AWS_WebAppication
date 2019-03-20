be2ce55d2d0d7b65d5dda0044dfa3e2c0b095250
Jingyi Cui <cui.jingy@husky.neu.edu>
2019-03-20 18:42:45 -0400

    Update

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -2,4 +2,5 @@
 sudo service tomcat stop
 kill -9 $(lsof -i:8080 | awk '{print $2}' | tail -n 2)
 cd /home/centos
-sudo chmod 777 -R CloudWebApp/target
+sudo mkdir -p CloubWebApp/traget/classes
+sudo chmod 777 -R CloudWebApp/target/classes
