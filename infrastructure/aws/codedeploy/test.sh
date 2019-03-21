db620d5187ca1cf83de9f1bbbf970e6b17ecfe0f
JinZhang-001899149 <zhang.jin2@husky.neu.edu>
2019-03-21 16:26:32 -0400

    urlchenage

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo service tomcat stop
-sudo kill -9 $(lsof -t -i:8080 | awk '{print $2}' | tail -n 2)
+sudo kill -9 $(sudo lsof -t -i:8080 | awk '{print $2}' | tail -n 2)
 cd /home/centos
 sudo mkdir -p CloudWebApp/target/BOOT-INF/classes
 sudo chmod 777 -R CloudWebApp/target/BOOT-INF/classes
