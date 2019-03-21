061e1cf0daf8f4e8a7c915585f37aab142da9c79
Jingyi Cui <cui.jingy@husky.neu.edu>
2019-03-21 19:18:33 -0400

    solve conflict

--- CloudWebApp/src/main/java/com/cloud/assignment/assignment/Note/NoteController.java
+++ CloudWebApp/src/main/java/com/cloud/assignment/assignment/Note/NoteController.java
@@ -84,7 +84,7 @@ public class NoteController {
     }
 
 
-    @PostMapping("/notejingyi")
+    @PostMapping("/note")
     public Object createNote(@RequestBody Note newNote, HttpServletResponse response, User newUser, @RequestHeader  String Authorization) {
 
 
