54ae6cb66994f4a48a92d2f7bf139a966c8969df
JinZhang-001899149 <zhang.jin2@husky.neu.edu>
2019-03-21 22:47:10 -0400

    update

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -3,5 +3,5 @@ sudo kill -9 $(sudo lsof -t -i:8080)
 sudo service tomcat stop
 sudo kill -9 $(sudo lsof -t -i:8080 | awk '{print $2}' | tail -n 2)
 cd /home/centos
-sudo mkdir -p CloudWebApp/target
 sudo chmod 777 -R CloudWebApp/target
+sudo chmod 777 -R /opt
