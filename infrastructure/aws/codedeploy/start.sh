88a0a031952751e85adcae34ea6871ca76320d7a
Yanjuan1995 <li.yanj@husky.neu.edu>
2019-03-23 03:37:17 -0400

    update

--- infrastructure/aws/codedeploy/start.sh
+++ infrastructure/aws/codedeploy/start.sh
@@ -3,7 +3,7 @@ cd /home/centos
 url=$(cat mysqlsetting.txt | sed -r 's/.*"(.+)".*/\1/')
 echo $url
 
-cd /home/centos/webapp/csye6225/target
+cd /home/centos/CloudWebApp/target
 pwd
 nohup java -jar assignment01-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.username=csye6225master --spring.datasource.password=csye6225password --spring.datasource.url=jdbc:mysql://$url/csye6225 --server.port=8080 >/opt/logs.log 2>&1 &
 
--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -3,5 +3,5 @@ sudo kill -9 $(sudo lsof -t -i:8080)
 sudo service tomcat stop
 sudo kill -9 $(sudo lsof -t -i:8080 | awk '{print $2}' | tail -n 2)
 cd /home/centos
-sudo chmod 777 -R webapp/csye6225/target
+sudo chmod 777 -R CloudWebApp/target
 sudo chmod 777 -R /opt
