702125893776a0bc3d6bfd417086eba2142ae581
JinZhang-001899149 <zhang.jin2@husky.neu.edu>
2019-03-25 20:13:04 -0400

    debug

--- infrastructure/aws/codedeploy/start.sh
+++ infrastructure/aws/codedeploy/start.sh
@@ -7,6 +7,7 @@ cd /home/centos/CloudWebApp/target
 pwd
 nohup java -jar assignment01-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.username=csye6225master --spring.datasource.password=csye6225password --spring.datasource.url=jdbc:mysql://$url/csye6225 --server.port=8080 >/opt/logs.log 2>&1 &
 
+
 cd /home/centos
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
